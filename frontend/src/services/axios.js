93f608d46b3cfadaa14b81070037e899ec743f35
wajeeha imran <wajeehan03@gmail.com>
2026-07-14 16:04:19 +0500

    Update axios.js

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://com-production-9fbe.up.railway.app/api",
 });
 
 API.interceptors.request.use((config) => {
